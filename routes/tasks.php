1464b0a6a52646abf046b171c1db003642132650
ezcodedeveloper <ezcodedeveloper@gmail.com>
2025-06-05 20:24:24 +0400

    Initial commit

--- routes/tasks.php
+++ routes/tasks.php
@@ -10,11 +10,4 @@ Route::middleware('auth')->group(function () {
     Route::get('tasks/get-tasks', [TaskController::class, 'taskDataTable'])->name('tasks.get-tasks');
     Route::delete('tasks/{id}', [TaskController::class, 'delete'])->name('tasks.delete');
     Route::put('tasks/update', [TaskController::class, 'update'])->name('patasksssword.update');
-
-    // Route::get('settings/password', [PasswordController::class, 'edit'])->name('password.edit');
-    // Route::put('settings/password', [PasswordController::class, 'update'])->name('password.update');
-
-    // Route::get('settings/appearance', function () {
-    //     return Inertia::render('settings/Appearance');
-    // })->name('appearance');
 });
